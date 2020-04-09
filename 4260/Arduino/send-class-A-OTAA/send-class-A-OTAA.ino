61ea80e830f815eca3649c4304255bce9a997129
nero.du <nero.du@rakwireless.com>
2020-04-09 11:21:13 +0800

    4260

---
+++ 4260/Arduino/send-class-A-OTAA/send-class-A-OTAA.ino
@@ -0,0 +1,99 @@
+/**
+ * Example of OTAA device      
+ * Authors: 
+ *        Ivan Moreno
+ *        Eduardo Contreras
+ *  June 2019
+ * 
+ * This code is beerware; if you see me (or any other collaborator 
+ * member) at the local, and you've found our code helpful, 
+ * please buy us a round!
+ * Distributed as-is; no warranty is given.
+ */
+
+#include <lorawan.h>
+
+
+// OTAA credentials
+const char *devEui = "00137a1000001478";
+const char *appEui = "00137a1000001478";
+//const char *appKey = "00137a100000147800137a1000001478";
+const char *appKey = "88000000000000000000000000000088";
+
+const unsigned long interval = 10000;    // 10 s interval to send message
+unsigned long previousMillis = 0;  // will store last time message sent
+unsigned int counter = 0;     // message counter
+
+char myStr[50];
+char outStr[255];
+byte recvStatus = 0;
+
+const sRFM_pins RFM_pins = {
+  .CS = SS,
+  .RST = RFM_RST,
+  .DIO0 = RFM_DIO0,
+  .DIO1 = RFM_DIO1,
+  .DIO2 = RFM_DIO2,
+  .DIO5 = RFM_DIO5,
+};
+
+void setup() {
+  // Setup loraid access
+  Serial.begin(115200);
+  while(!Serial);
+
+  if(!lora.init()){
+    Serial.println("RFM95 not detected");
+    delay(5000);
+    return;
+  }
+
+  // Set LoRaWAN Class change CLASS_A or CLASS_C
+  lora.setDeviceClass(CLASS_A);
+
+  // Set Data Rate
+  lora.setDataRate(SF8BW125);
+
+  // set channel to random
+  lora.setChannel(MULTI);
+  
+  // Put OTAA Key and DevAddress here
+  lora.setDevEUI(devEui);
+  lora.setAppEUI(appEui);
+  lora.setAppKey(appKey);
+
+  // Join procedure
+  bool isJoined;
+  do {
+    Serial.println("Joining...");
+
+    isJoined = lora.join();
+    
+    //wait for 10s to try again
+    delay(10000);
+  }while(!isJoined);
+  Serial.println("Joined to network");
+}
+
+void loop() {
+  // Check interval overflow
+  if(millis() - previousMillis > interval) {
+    previousMillis = millis(); 
+
+    sprintf(myStr, "Counter-%d", counter); 
+
+    Serial.print("Sending: ");
+    Serial.println(myStr);
+    
+    lora.sendUplink(myStr, strlen(myStr), 0);
+    counter++;
+  }
+
+  recvStatus = lora.readData(outStr);
+  if(recvStatus) {
+    Serial.println(outStr);
+  }
+  
+  // Check Lora RX
+  lora.update();
+}
