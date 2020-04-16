8dd3b9432fc0440f2284580997a44a9aafc7bec8
nero.du <nero.du@rakwireless.com>
2020-04-16 14:19:19 +0800

    abp

--- RAK811/Arduino_RAK811_LoRaWAN/Arduino_RAK811_LoRaWAN.ino
+++ RAK811/Arduino_RAK811_LoRaWAN/Arduino_RAK811_LoRaWAN.ino
@@ -1,238 +1,92 @@
-/*******************************************************************************
- * Copyright (c) 2015 Thomas Telkamp and Matthijs Kooijman
- * Copyright (c) 2018 Terry Moore, MCCI
- *
- * Permission is hereby granted, free of charge, to anyone
- * obtaining a copy of this document and accompanying files,
- * to do whatever they want with them without any restriction,
- * including, but not limited to, copying, modification and redistribution.
- * NO WARRANTY OF ANY KIND IS PROVIDED.
- *
- * This example sends a valid LoRaWAN packet with payload "Hello,
- * world!", using frequency and encryption settings matching those of
- * the The Things Network.
- *
- * This uses OTAA (Over-the-air activation), where where a DevEUI and
- * application key is configured, which are used in an over-the-air
- * activation procedure where a DevAddr and session keys are
- * assigned/generated for use with all further communication.
- *
- * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
- * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
- * violated by this sketch when left running for longer)!
-
- * To use this sketch, first register your application and device with
- * the things network, to set or generate an AppEUI, DevEUI and AppKey.
- * Multiple devices can use the same AppEUI, but each device has its own
- * DevEUI and AppKey.
- *
- * Do not forget to define the radio type correctly in
- * arduino-lmic/project_config/lmic_project_config.h or from your BOARDS.txt.
- *
- *******************************************************************************/
-
-#include <lmic.h>
-#include <hal/hal.h>
+// Hello LoRa - ABP TTN Packet Sender (Multi-Channel)
+// Tutorial Link: https://learn.adafruit.com/the-things-network-for-feather/using-a-feather-32u4
+//
+// Adafruit invests time and resources providing this open source code.
+// Please support Adafruit and open source hardware by purchasing
+// products from Adafruit!
+//
+// Copyright 2015, 2016 Ideetron B.V.
+//
+// Modified by Brent Rubell for Adafruit Industries, 2018
+/************************** Configuration ***********************************/
+#include <TinyLoRa.h>
 #include <SPI.h>
 
-//
-// For normal use, we require that you edit the sketch to replace FILLMEIN
-// with values assigned by the TTN console. However, for regression tests,
-// we want to be able to compile these scripts. The regression tests define
-// COMPILE_REGRESSION_TEST, and in that case we define FILLMEIN to a non-
-// working but innocuous value.
-//
-//#ifdef COMPILE_REGRESSION_TEST
-//# define FILLMEIN 0
-//#else
-//# warning "You must replace the values marked FILLMEIN with real values from the TTN control panel!"
-//# define FILLMEIN (#dont edit this, edit the lines that use FILLMEIN)
-//#endif
-int counter = 0;
-// This EUI must be in little-endian format, so least-significant-byte
-// first. When copying an EUI from ttnctl output, this means to reverse
-// the bytes. 
-static const u1_t PROGMEM APPEUI[8]={0x1A,0xFE,0x01,0xD0,0x7E,0xD5,0xB3,0x70};
-void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
+// Visit your thethingsnetwork.org device console
+// to create an account, or if you need your session keys.
 
-// This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]={0xA2,0xAA,0xAA,0xAA,0xAA,0xAA,0xAA,0xAA};
-void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
+// Network Session Key (MSB)
+uint8_t NwkSkey[16] = { 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11 };
 
-// This key should be in big endian format (or, since it is not really a
-// number but a block of memory, endianness does not really apply). In
-// practice, a key taken from ttnctl can be copied as-is.
-static const u1_t PROGMEM APPKEY[16] = { 0x45,0xBA,0x29,0x8E,0x71,0xFA,0xA5,0x33,0xFA,0xBC,0xD6,0x89,0xBE,0xB6,0xC7,0x27 };
-void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
+// Application Session Key (MSB)
+uint8_t AppSkey[16] = { 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11 };
 
-static uint8_t lora_data[64];
-uint32_t lora_count = 0;
-static osjob_t sendjob;
+// Device Address (MSB)
+uint8_t DevAddr[4] = { 0x11, 0x11, 0x11, 0x11 };
 
-typedef enum {
-  LORA_UNCONFIRMED,
-  LORA_CONFIRMED  
-};
-// Schedule TX every this many seconds (might become longer due to duty
-// cycle limitations).
-const unsigned TX_INTERVAL = 10;
+/************************** Example Begins Here ***********************************/
+// Data Packet to Send to TTN
+unsigned char loraData[11] = {"hello LoRa"};
 
-// Pin mapping
-const lmic_pinmap lmic_pins = {
-    .nss = 26,
-    .rxtx = 32,
-    .rst = 21,
-    .dio = {27, 28, 29},
-};
+// How many times data transfer should occur, in seconds
+const unsigned int sendInterval = 10;
 
-void onEvent (ev_t ev) {
-    //Serial.print(os_getTime());
-    //Serial.print(": ");
-    switch(ev) {
-        case EV_SCAN_TIMEOUT:
-            Serial.println(F("EV_SCAN_TIMEOUT"));
-            break;
-        case EV_BEACON_FOUND:
-            Serial.println(F("EV_BEACON_FOUND"));
-            break;
-        case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
-            break;
-        case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
-            break;
-        case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
-            break;
-        case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
-            {
-              u4_t netid = 0;
-              devaddr_t devaddr = 0;
-              u1_t nwkKey[16];
-              u1_t artKey[16];
-              LMIC_getSessionKeys(&netid, &devaddr, nwkKey, artKey);
-              Serial.print("netid: ");
-              Serial.println(netid, DEC);
-              Serial.print("devaddr: ");
-              Serial.println(devaddr, HEX);
-              Serial.print("artKey: ");
-              for (size_t i=0; i<sizeof(artKey); ++i) {
-                Serial.print(artKey[i], HEX);
-              }
-              Serial.println("");
-              Serial.print("nwkKey: ");
-              for (size_t i=0; i<sizeof(nwkKey); ++i) {
-                Serial.print(nwkKey[i], HEX);
-              }
-              Serial.println("");
+// Pinout for Adafruit Feather 32u4 LoRa
+TinyLoRa lora = TinyLoRa(27, 26, 21);
 
-              lora_count = 1;
-            }
-            // Disable link check validation (automatically enabled
-            // during join, but because slow data rates change max TX
-            // size, we don't use it in this example.
-            LMIC_setLinkCheckMode(0);
-            break;
-        /*
-        || This event is defined but not used in the code. No
-        || point in wasting codespace on it.
-        ||
-        || case EV_RFU1:
-        ||     Serial.println(F("EV_RFU1"));
-        ||     break;
-        */
-        case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
-            break;
-        case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
-            break;
-        case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack\r\n"));
-            if (LMIC.dataLen) {
-              Serial.print(F("Received "));
-              Serial.print(LMIC.dataLen);
-              Serial.println(F(" bytes of payload"));
-            }
-            // Schedule next transmission
-            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-            break;
-        case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
-            break;
-        case EV_RESET:
-            Serial.println(F("EV_RESET"));
-            break;
-        case EV_RXCOMPLETE:
-            // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
-            break;
-        case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
-            break;
-        case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
-            break;
-        /*
-        || This event is defined but not used in the code. No
-        || point in wasting codespace on it.
-        ||
-        || case EV_SCAN_FOUND:
-        ||    Serial.println(F("EV_SCAN_FOUND"));
-        ||    break;
-        */
-        case EV_TXSTART:
-            Serial.println(F("EV_TXSTART"));
-            break;
-        default:
-            Serial.print(F("Unknown event: "));
-            Serial.println((unsigned) ev);
-            break;
-    }
-}
+// Pinout for Adafruit Feather M0 LoRa
+//TinyLoRa lora = TinyLoRa(3, 8, 4);
 
-void do_send(osjob_t* j){
-    // Check if there is not a current TX/RX job running
-    if (LMIC.opmode & OP_TXRXPEND) {
-        Serial.println(F("OP_TXRXPEND, not sending"));
-    } else {
-        // Prepare upstream data transmission at the next possible time.
-        sprintf((char *)lora_data, "Hello_%d", lora_count++);
-        LMIC_setTxData2(1, lora_data, strlen((char *)lora_data), LORA_UNCONFIRMED);
-        Serial.println(F("Packet queued"));
-    }
-    // Next TX is scheduled after TX_COMPLETE event.
-}
-
-void setup() {
-    Serial.begin(115200);
-    while ( !Serial ) delay(10);
-    Serial.println("\r\n*******************************");
-    Serial.println("System Starting...");
-    Serial.println("");
-    
-    pinMode(RADIO_XTAL_EN,OUTPUT);
+void setup()
+{
+  delay(2000);
+  Serial.begin(115200);
+  while (! Serial);
+  
+      pinMode(RADIO_XTAL_EN,OUTPUT);
     digitalWrite(RADIO_XTAL_EN,1);
+    
+  // Initialize pin LED_BUILTIN as an output
+  //pinMode(LED_BUILTIN, OUTPUT);
+  
+  // Initialize LoRa
+  Serial.print("Starting LoRa...");
+  // define multi-channel sending
+  lora.setChannel(MULTI);
+  // set datarate
+  lora.setDatarate(SF7BW125);
+  if(!lora.begin())
+  {
+    Serial.println("Failed");
+    Serial.println("Check your radio");
+    while(true);
+  }
 
+  // Optional set transmit power. If not set default is +17 dBm.
+  // Valid options are: -80, 1 to 17, 20 (dBm).
+  // For safe operation in 20dBm: your antenna must be 3:1 VWSR or better
+  // and respect the 1% duty cycle.
 
-    // LMIC init
-    os_init();
-    // Reset the MAC state. Session and pending data transfers will be discarded.
-    LMIC_reset();
+  // lora.setPower(17);
 
-#if defined(CFG_us915)
-    LMIC_selectSubBand(1);
-#endif
-    LMIC_setAdrMode(true);  // ADR
-    LMIC_setDrTxpow(DR_SF7, 14);  // DR TxPower
-
-    // Start job (sending automatically starts OTAA too)
-    do_send(&sendjob);
+  Serial.println("OK");
 }
 
-void loop() {
-    os_runloop_once();
+void loop()
+{
+  Serial.println("Sending LoRa Data...");
+  lora.sendData(loraData, sizeof(loraData), lora.frameCounter);
+  // Optionally set the Frame Port (1 to 255)
+  // uint8_t framePort = 1;
+  // lora.sendData(loraData, sizeof(loraData), lora.frameCounter, framePort);
+  Serial.print("Frame Counter: ");Serial.println(lora.frameCounter);
+  lora.frameCounter++;
+
+  // blink LED to indicate packet sent
+//  digitalWrite(LED_BUILTIN, HIGH);
+//  delay(1000);
+//  digitalWrite(LED_BUILTIN, LOW);
+  
+  Serial.println("delaying...");
+  delay(sendInterval * 1000);
 }
